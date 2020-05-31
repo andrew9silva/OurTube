a387da1f63bc4002d19037022d531faf95711a33
andrewsilva9 <andrewsilva9@gmail.com>
2020-05-31 12:40:59 -0600

    fixed column name in videos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_200948) do
+ActiveRecord::Schema.define(version: 2020_05_31_182255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_200948) do
     t.string "title"
     t.string "url"
     t.text "description"
-    t.integer "uer_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
