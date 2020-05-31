0c54cc9a980bb4e817b445b276d6c21bdca06741
andrewsilva9 <andrewsilva9@gmail.com>
2020-05-31 16:33:24 -0600

    added video#create route and controller with strong params

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::VideosController < ApplicationController
         videos = Video.all 
         render json: videos
     end
+
+    def create
+        video = Video.new(video_params)
+        if video.save
+            render json: video, status: :accepted
+        else
+            render json: {errors: video.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def video_params
+        
+        params.require(:video).permit(:title, :url, :description, :user_id)
+    end
 end
