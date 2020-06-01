1d1ccf111baf81f3ab6f3a2e6c62a1bcec5661cc
andrewsilva9 <andrewsilva9@gmail.com>
2020-06-01 14:57:42 -0600

    got CORS working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+user = User.create(username: "test", email: "test@test.com", password_digest: "password")
+Video.create(title: "Test Video", url: "https://www.youtube.com/watch?v=2xvuGWI3H58&feature=youtu.be", description: "I hope this works.", user_id: user.id)
