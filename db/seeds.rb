875ebef4e4b34b82cb6e41a428c20bdb8cd7cccb
andrewsilva9 <andrewsilva9@gmail.com>
2020-06-07 13:51:18 -0600

    added more sed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,4 @@
 
 user = User.create(username: "test", email: "test@test.com", password_digest: "password")
 Video.create(title: "Test Video", url: "https://www.youtube.com/watch?v=C_n1R_Sfl3w", description: "I hope this works.", user_id: user.id)
+Video.create(title: "Baseball", url: "https://www.youtube.com/watch?v=8EjI-6hep8Y", description: "Baseball Highlights", user_id: user.id)
